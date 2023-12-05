08b4c3f359aba2187baa18f2cbfabb8a914db771
Thiene-L <qq1031828119@gmail.com>
2023-12-04 21:49:58 -0600

    Initial commit

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ mongoose.connection.on('open', function (err) {
             cookie: {
                 httpOnly: true, // 开启后前端无法通过 JS 操作cookie
                 maxAge: 24 * 3600 * 1000 // 设置cookie的过期时间,cookie的key，cookie的value，均不在此处配置。
+                sameSite: 'none'
             },
         }));
         const allowedOrigins = ['http://localhost:4200', 'https://bl-hw7.surge.sh', 'http://localhost:3000', 'https://accounts.google.com'];
